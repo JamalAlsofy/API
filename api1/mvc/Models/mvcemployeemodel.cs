17983381c84f76bc9032ac77b364cdd08ff6bb0b
JamalAlsofy <107511283+JamalAlsofy@users.noreply.github.com>
2022-08-29 00:02:30 +0300

    init

---
+++ api1/mvc/Models/mvcemployeemodel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace mvc.Models
+{
+    public class mvcemployeemodel
+    {
+        public int EmployeelID { get; set; }
+        [Required(ErrorMessage ="This Field is Required")]
+        public string Name { get; set; }
+        public Nullable<int> Position { get; set; }
+        public Nullable<int> Age { get; set; }
+        public Nullable<int> Salary { get; set; }
+    }
+}
